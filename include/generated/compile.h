1499707fab98af1d8f008c8c67aa7eb9c1d413b4
yu <yu@m.y-log.net>
2012-04-05 03:32:52 +0900

    add L-06C v10r config & cifs, tun etc

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Thu Apr 5 04:02:04 JST 2012"
+#define LINUX_COMPILE_TIME "04:02:04"
+#define LINUX_COMPILE_BY "user"
+#define LINUX_COMPILE_HOST "Ubuntu-VM11"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
